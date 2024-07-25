1b34a8ea131bf3082629dd9618f62926af9cde6a
toum21 <pey2moo1@gmail.com>
2024-07-25 11:24:03 -0700

    added jsdom to the project

--- crawl.js
+++ crawl.js
@@ -1,5 +1,6 @@
 function noramlizeUrl(urlString) {
     // beacuse the url constructor already knows that urls do not care for upper case a nd lower case the capitals test is passed without any logic to do that 
+    // ^ same goes for the http or https test
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
