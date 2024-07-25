182d1f7b2f94bf82fbe752a633cdd154629941d9
toum21 <pey2moo1@gmail.com>
2024-07-25 11:18:33 -0700

    renamed the tests to make them easier to read and more organized

--- crawl.test.js
+++ crawl.test.js
@@ -2,15 +2,22 @@ const noramlizeUrl = require('./crawl').noramlizeUrl;
 const { test, expect } = require('@jest/globals');
 const exp = require('constants');
 
-test('noramlizeUrl', () => {
+test('noramlizeUrl strip protocol', () => {
     const input = 'https://blog.boot.dev/path'
     const actual = noramlizeUrl(input);
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected);
 });
 
-test('noramlizeUrl', () => {
+test('noramlizeUrl strip trailing slash', () => {
     const input = 'https://blog.boot.dev/path/'
+    const actual = noramlizeUrl(input);
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected);
+});
+
+test('noramlizeUrl capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
     const actual = noramlizeUrl(input);
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected);
