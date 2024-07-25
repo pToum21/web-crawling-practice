ebc502238cc6107019506ce707a1b9a19a69cd15
toum21 <pey2moo1@gmail.com>
2024-07-25 11:22:31 -0700

    added comments for each test as well as a comment in crawl as to why the capital and https test pass automatically without an specfic logic for them to

--- crawl.js
+++ crawl.js
@@ -1,4 +1,5 @@
 function noramlizeUrl(urlString) {
+    // beacuse the url constructor already knows that urls do not care for upper case a nd lower case the capitals test is passed without any logic to do that 
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
     if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
--- crawl.test.js
+++ crawl.test.js
@@ -2,6 +2,7 @@ const noramlizeUrl = require('./crawl').noramlizeUrl;
 const { test, expect } = require('@jest/globals');
 const exp = require('constants');
 
+// test to strip the protocol
 test('noramlizeUrl strip protocol', () => {
     const input = 'https://blog.boot.dev/path'
     const actual = noramlizeUrl(input);
@@ -9,6 +10,7 @@ test('noramlizeUrl strip protocol', () => {
     expect(actual).toEqual(expected);
 });
 
+// test to strip the trailing slash
 test('noramlizeUrl strip trailing slash', () => {
     const input = 'https://blog.boot.dev/path/'
     const actual = noramlizeUrl(input);
@@ -16,8 +18,17 @@ test('noramlizeUrl strip trailing slash', () => {
     expect(actual).toEqual(expected);
 });
 
+// test to omit capital letters
 test('noramlizeUrl capitals', () => {
     const input = 'https://BLOG.boot.dev/path/'
+    const actual = noramlizeUrl(input);
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected);
+});
+
+// test to strip the http or https
+test('noramlizeUrl strip http', () => {
+    const input = 'http://BLOG.boot.dev/path/'
     const actual = noramlizeUrl(input);
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected);
