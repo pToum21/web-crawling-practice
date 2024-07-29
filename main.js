e38ada87b2755e0c905e636994d83f1b38469803
toum21 <pey2moo1@gmail.com>
2024-07-28 17:38:26 -0700

    working on the main js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+function main() {
+    // we use three because the length of the array is 3 and the first two are the node and the file path
+    if (process.argv.length < 3) {
+        console.log('no website provided');
+        process.exit(1);
+    }
+    for (const arg of process.argv) {
+        console.log(arg);
+    }
+
+    console.log("starting to crawl website");
+}
+
+main();
